cb5586faf45b618c22373850ab41a9eb2efcc4b3
Erika Bueno <erikab@ciandt.com>
2023-01-04 18:32:08 -0300

    Initial Commit

---
+++ SDKLogger/SDKLogger.h
@@ -0,0 +1,18 @@
+//
+//  SDKLogger.h
+//  SDKLogger
+//
+//  Created by Erika C. Matesz Bueno on 04/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKLogger.
+FOUNDATION_EXPORT double SDKLoggerVersionNumber;
+
+//! Project version string for SDKLogger.
+FOUNDATION_EXPORT const unsigned char SDKLoggerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKLogger/PublicHeader.h>
+
+
